10cb707bc3f33ae6682e27aca88f9b331dff712e
huaxingCao <48424044+huaxingCao@users.noreply.github.com>
2022-09-14 18:32:33 +0800

    Update SingleLinkedList.java

--- src/main/java/com/chx/datastructure/linearstructure/SingleLinkedList.java
+++ src/main/java/com/chx/datastructure/linearstructure/SingleLinkedList.java
@@ -6,7 +6,7 @@ public class SingleLinkedList<E> {
     /**
      * 单链表长度
      */
-    private int length;
+    public int length;
 
     public SingleLinkedList() {}
 
@@ -65,30 +65,106 @@ public class SingleLinkedList<E> {
     }
 
     /**
+     * 末尾添加元素
+     * @param element 元素
+     * @return 是否成功
+     */
+    public boolean add(E element) {
+        return insert(this.length, element);
+    }
+
+    /**
      * 在下标i的位置插入元素，时间复杂度是 O(n)
-     * @param index 下标
+     * @param i 下标
      * @param element 要插入的元素
      * @return 是否成功
      */
-    public boolean insert(int index, E element) {
+    public boolean insert(int i, E element) {
         boolean res = false;
         Node<E> newNode = new Node<>();
         newNode.setElement(element);
-        if (index == 0) {
+        if (i == 0) {
             newNode.setNext(this.elements);
             this.elements = newNode;
+            this.length += 1;
             res = true;
-        } else if (index > 0 && index <= this.length) {
-            Node<E> oldNode = findNode(index - 1);
+        } else if (i > 0 && i <= this.length) {
+            Node<E> oldNode = findNode(i - 1);
             newNode.setNext(oldNode.getNext());
             oldNode.setNext(newNode);
+            this.length += 1;
             res = true;
         }
 
         return res;
     }
 
-    //public boolean delete(int i) {}
+    /**
+     * 删除下标i的元素，时间复杂度为 O(n)
+     * @param i 下标
+     * @return 是否成功
+     */
+    public boolean delete(int i) {
+        boolean res = false;
+        if (i == 0 ) {
+            this.elements = this.elements.getNext();
+            this.length -= 1;
+            res = true;
+        } else if (i > 0 && i < this.length) {
+            Node<E> lastNode = findNode(i - 1);
+            Node<E> node = findNode(i);
+            lastNode.setNext(node.getNext());
+            this.length -= 1;
+            res = true;
+        }
 
-    //public boolean update(int i, E element) {}
+        return res;
+    }
+
+    /***
+     * 更新下标为index的元素，时间复杂度为O(n)
+     * @param index 下标
+     * @param element 元素
+     * @return 是否成功
+     */
+    public boolean update(int index, E element) {
+        boolean res = false;
+        if (index >= 0 && index < this.length) {
+            findNode(index).setElement(element);
+        }
+
+        return res;
+    }
+
+    public void test() {
+        System.out.println(this.length);
+
+        if (this.elements != null) {
+            Node<E> elements = this.elements;
+            System.out.print(elements.getElement());
+            while (elements.getNext() != null) {
+                elements = elements.getNext();
+                System.out.print(" ");
+                System.out.print(elements.getElement());
+            }
+        }
+
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList<String> list = new SingleLinkedList<>();
+
+        list.add("a");
+        list.add("b");
+        list.test();
+
+        list.insert(2, "c");
+        list.test();
+
+        list.update(1, "d");
+        list.test();
+
+
+    }
 }
